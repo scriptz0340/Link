7244594fdc6f281b30e0b2cedf1e197c0bbe391b
scriptz0340 <228908614+scriptz0340@users.noreply.github.com>
2026-09-15 14:30:31 -0700

    Fixed the demo flet app.

--- src/flet-demo/trivia.py
+++ src/flet-demo/trivia.py
@@ -3,7 +3,7 @@ import flet as ft
 def main(page: ft.Page):
     challenge = ft.Text("Dating back to a 1974 Bell Labs memo, what two-word greeting is universally known as the classic first program written in almost every language?", size=30)
     
-    def hint():
+    def hint_button_click():
         page.add(
             ft.SafeArea(
                 content=ft.AlertDialog(
@@ -14,13 +14,18 @@ def main(page: ft.Page):
             )
         )
 
+    def submit_button_click(e: ft.Event[ft.Button]):
+        evaluate(response_field.value)
+        
+        
+
     def evaluate(e):  
         if e == 'Hello, world!':
             page.add(
                 ft.SafeArea(
                     content=ft.AlertDialog(
-                        title=ft.Text("Congratulations!"),
-                        content=ft.Text("You guessed correctly! \n\n(Click off to close)"),
+                        title=ft.Text("Congratulations! 🎉"),
+                        content=ft.Text("You guessed correctly! 🥳 \n\n While most people associate it with the C programming language, it was actually first written in B (C's predecessor) in a 1972 internal memo titled A Tutorial Introduction to the Language B.\n\nIt became a global tradition after it was featured as the opening example in the seminal 1978 book, The C Programming Language, co-authored by Kernighan and Dennis Ritchie. \n\n(Click off to close)"),
                         open=True,
                     )
                 )
@@ -30,8 +35,8 @@ def main(page: ft.Page):
             page.add(
                 ft.SafeArea(
                     content=ft.AlertDialog(
-                        title=ft.Text("Incorrect."),
-                        content=ft.Text("Please try again.\n\n(Click off to close)"),
+                        title=ft.Text("Incorrect. 😢"),
+                        content=ft.Text("Please try again. 🔄\n\n(Click off to close)"),
                         open=True,
                     )
                 )
@@ -43,6 +48,7 @@ def main(page: ft.Page):
     page.add(
         ft.SafeArea(
             expand=True,
+
             content=ft.Container(
                 content=challenge,
                 alignment=ft.Alignment.CENTER,
@@ -53,29 +59,23 @@ def main(page: ft.Page):
 
     page.add(
         ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content = ft.TextField(label="Response", hint_text="Enter your response.", autocorrect=False, show_cursor=True, on_submit=evaluate),
-                
-                alignment=ft.Alignment.CENTER,
-                
-            )
-        )
-    )
-
-    page.add(
-        ft.SafeArea(
             content=ft.Row(
-                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                 controls=[
-                    ft.FilledButton(content="Hint", on_click=hint),
-                    ft.FloatingActionButton(icon=ft.Icons.POST_ADD, on_click=evaluate)
+                    response_field := ft.TextField(
+                        key="response_field",
+                        label="Response",
+                        on_submit=submit_button_click,
+                    ),
+                    ft.Button(key="submit_button", content="Submit", on_click=submit_button_click),
+                    ft.Button(key="hint_button", content="Hint", on_click=hint_button_click),
+                    message := ft.Text()
 
                 ]
+    
             )
-            
         )
     )
+
 
 if __name__ == '__main__': # __name__ is a built in variable. when you run a file directly, python sets this variable to the text __main__. When another file imports this file, Python sets it to the file name instead. This checks to make sure you are calling the file directly.
     ft.run(main)
